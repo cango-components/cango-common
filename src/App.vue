061d3b1a64ca0762ae7a5f7a2f678cc88104d435
liululu0320 <1101483280@qq.com>
2018-10-15 15:57:05 +0800

    '修改树'

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,63 @@
 <template>
   <div>
     <cg-form ref = "form" >
+      <cg-button
+        :className = "button.className"
+        :label = "button.label"
+        :readonly = "button.readOnly"
+        :buttonclick = "button.buttonClick"
+      >
+      </cg-button>
+      <cg-tree
+        :showname="tree.showName"
+        :childname="tree.childName"
+        :showfunc="tree.showFunc"
+        :list="tree.list"
+        :onselected="tree.onSelected"
+        :onextend="tree.onExtend"
+        :filter="tree.filter"
+        :filterignore="tree.filterIgnore"
+        :filterplaceholder="tree.filterPlaceholder"
+      >
+      </cg-tree>
+      <cg-select
+        :label = "select.label"
+        :id = "select.id"
+        :readonly = "select.readOnly"
+        :required = "select.required"
+        :emptyisnull = "select.emptyIsNull"
+        :keyname = "select.keyName"
+        :showname = "select.showName"
+        :list = "select.list"
+        :showfunc = "select.showFunc"
+        :filter = "select.filter"
+        :filterignore = "select.filterIgnore"
+        :filterplaceholder = "select.filterPlaceholder"
+        :emptylabel = "select.emptyLabel"
+        :selectnum = "select.selectNum"
+        :separator = "select.separator"
+        :titlestyle = "select.titleStyle"
+        v-model = "data.select"
+      >
+      </cg-select>
       <cg-uploadify
-        id       = "id"
-        type     = "video"
-        label    = ""
-        :required = "true"
-        prefix   = "TEST"
-        v-model = "val"
+        :type = "uploadify.type"
+        :id = "uploadify.id"
+        :readonly = "uploadify.readOnly"
+        :candelete = "uploadify.canDelete"
+        :required = "uploadify.required"
+        :emptyisnull = "uploadify.emptyIsNull"
+        :prefix = "uploadify.prefix"
+        :filenum = "uploadify.fileNum"
+        :maxsize = "uploadify.maxSize"
+        :errorimage = "uploadify.errorImage"
+        :defaultimage = "uploadify.defaultImage"
+        :loadingimage = "uploadify.loadingImage"
+        v-model = "data.uploadify"
       >
       </cg-uploadify>
     </cg-form>
+
   </div>
 </template>
 
@@ -20,8 +67,57 @@ export default {
   name: 'App',
   data () {
     return {
-      updateTime: null,
-      val: ''
+      button: {
+        className: '',
+        label: 'button',
+        readOnly: false,
+        buttonClick: null
+      },
+      select: {
+        id: 'select',
+        label: '',
+        readOnly: false,
+        required: false,
+        emptyIsNull: true,
+        keyName: 'id',
+        showName: 'name',
+        showFunc: null,
+        list: [{"id":"id1", "name":"名称1"},
+          {"id":"id2", "name":"名称2"}],
+        filter: true,
+        filterIgnore: true,
+        filterPlaceholder: '请输入关键字',
+        emptyLabel: '请选择',
+        separator: ',',
+        selectNum: 1,
+        titleStyle: 0
+      },
+      tree: {
+        showName: 'name',
+        childName: 'child',
+        showFunc: null,
+        list: [{"name":"父一","child":[{"name":"子一"}]}, {"name":"父二"}, {"name":"父三"}],
+        onSelected: null,
+        onExtend: null,
+        filter: false,
+        filterIgnore: true,
+        filterPlaceholder: '请选择'
+      },
+      data:{
+        select:null
+      },
+      uploadify: {
+        id: 'uploadifyId',
+        label: 'uploadify',
+        type: 'video',
+        prefix: 'Test',
+        fileNum: 0,
+        maxSize: 0,
+        required: false,
+        readOnly: false,
+        canDelete: true,
+        emptyIsNull: true
+      },
     }
   },
   methods: {
--- src/components/cgTree/src/main.vue
+++ src/components/cgTree/src/main.vue
@@ -7,12 +7,12 @@
       <div v-for='(node, nodeIndex) in getList'
            :key='"node" + nodeIndex'
            class = 'cg-tree__node' :title="showRecordName(node.record)">
-        <div  :class = '(node == selectNode) ? "cg-tree__selected" : ""'   @click='treeNodeExtend(node)'>
-          <span v-for="i in node.level" :key='"node" + nodeIndex + "span" + i'>&nbsp;&nbsp;</span>
-          <i class='cango-uploadify__file_iconfont icon-zengjia'></i>
-            <slot v-bind:option='node' >
-              {{ showRecordName(node.record) }}
-            </slot>
+        <div  :class = '(node == selectNode) ? "cg-tree__selected" : ""' >
+          <span v-for="i in node.level" :key='"node" + nodeIndex + "span" + i' >&nbsp;&nbsp;</span>
+          <i class='cango-uploadify__file_iconfont icon-zengjia' @click='treeNodeExtend(node)'></i>
+          <span v-bind:option='node' @click="treeNodeSelect(node)">
+            {{ showRecordName(node.record) }}
+          </span>
         </div>
       </div>
     </div>
@@ -20,202 +20,201 @@
 </template>
 
 <script>
-export default {
-  name: 'cg-tree',
-  props: {
-    // 和data配合使用，去data里面对应的showname的值作为显示(-做分割进行深层次访问)
-    'showname': {
-      type: String,
-      default: 'name'
-    },
-    // 和data配合使用，去data里面对应的childname的值作为子节点列表(-做分割进行深层次访问)
-    'childname': {
-      type: String,
-      default: 'child'
-    },
-    // 展示的函数，优先级再showname之前，复杂展示方式的时候使用，这里考虑用插槽实现
-    'showfunc': {
-      type: Function
-    },
-    // 候选项
-    'list': {
-      type: Array,
-      default: function () {
-        return []
-      }
-    },
-    // 选中事件
-    'onselected': {
-      type: Function
-    },
-    // 展开事件
-    'onextend': {
-      type: Function
-    },
-    // 是否显示筛选器(在已知的节点范围内进行检索，最好配合list一次性加载，否则只检索已知数据)
-    'filter': {
-      type: Boolean,
-      default: false
-    },
-    // 是否忽略大小写
-    'filterignore': {
-      type: Boolean,
-      default: true
-    },
-    // 筛选器默认文字
-    'filterplaceholder': {
-      type: String,
-      default: '请选择'
-    }
-  },
-  created: function () {
-    // 刷新树图
-    this.recordList = this.resizeRecordList(this.list, 1, false)
-  },
-  data: function () {
-    return {
-      recordList: [],
-      filterText: '',
-      selectNode: null
-    }
-  },
-  computed: {
-    getList: function () {
-      let finalList = []
-      if (this.recordList) {
-        let preLevel = 0
-        let preShowChild = true
-        for (let i = 0; i < this.recordList.length; i++) {
-          if (this.recordList[i].level > preLevel) {
-            if (!preShowChild) {
-              continue
-            }
-            finalList.push(this.recordList[i])
-          } else {
-            finalList.push(this.recordList[i])
-          }
-          preLevel = this.recordList[i].level
-          preShowChild = this.recordList[i].showChild
+  export default {
+    name: 'cg-tree',
+    props: {
+      // 和data配合使用，去data里面对应的showname的值作为显示(-做分割进行深层次访问)
+      'showname': {
+        type: String,
+        default: 'name'
+      },
+      // 和data配合使用，去data里面对应的childname的值作为子节点列表(-做分割进行深层次访问)
+      'childname': {
+        type: String,
+        default: 'child'
+      },
+      // 展示的函数，优先级再showname之前，复杂展示方式的时候使用，这里考虑用插槽实现
+      'showfunc': {
+        type: Function
+      },
+      // 候选项
+      'list': {
+        type: Array,
+        default: function () {
+          return []
         }
+      },
+      // 选中事件
+      'onselected': {
+        type: Function
+      },
+      // 展开事件
+      'onextend': {
+        type: Function
+      },
+      // 是否显示筛选器(在已知的节点范围内进行检索，最好配合list一次性加载，否则只检索已知数据)
+      'filter': {
+        type: Boolean,
+        default: false
+      },
+      // 是否忽略大小写
+      'filterignore': {
+        type: Boolean,
+        default: true
+      },
+      // 筛选器默认文字
+      'filterplaceholder': {
+        type: String,
+        default: '请选择'
       }
-      return finalList
-    }
-  },
-  methods: {
-    filterLike: function (record) {
-      if (this.filterText === '') return true
-      let filterText = this.filterText + ''
-      let text = this.showRecordName(record) + ''
-      if (this.filterignore) {
-        filterText = filterText.toLowerCase()
-        text = text.toLowerCase()
+    },
+    created: function () {
+      // 刷新树图
+      this.recordList = this.resizeRecordList(this.list, 1, false)
+    },
+    data: function () {
+      return {
+        recordList: [],
+        filterText: '',
+        selectNode: null
       }
-      if (text.indexOf(filterText) >= 0) {
-        return true
-      } else {
+    },
+    computed: {
+      getList: function () {
+        let finalList = []
+        if (this.recordList) {
+          let preLevel = 0
+          let preShowChild = true
+          for (let i = 0; i < this.recordList.length; i++) {
+            if (this.recordList[i].level > preLevel) {
+              if (!preShowChild) {
+                continue
+              }
+              finalList.push(this.recordList[i])
+            } else {
+              finalList.push(this.recordList[i])
+            }
+            preLevel = this.recordList[i].level
+            preShowChild = this.recordList[i].showChild
+          }
+        }
+        return finalList
+      }
+    },
+    methods: {
+      filterLike: function (record) {
+        if (this.filterText === '') return true
+        let filterText = this.filterText + ''
+        let text = this.showRecordName(record) + ''
+        if (this.filterignore) {
+          filterText = filterText.toLowerCase()
+          text = text.toLowerCase()
+        }
+        if (text.indexOf(filterText) >= 0) {
+          return true
+        } else {
+          return false
+        }
+      },
+
+      showRecordName: function (record) {
+        if (this.showfunc) {
+          return this.showfunc(record)
+        } else {
+          return record[this.showname]
+        }
+      },
+      treeNodeExtend: function (record) {
+        record.showChild = !record.showChild
+        if (this.onextend) {
+          this.onextend(record.record, record.showChild)
+        }
+      },
+      treeNodeSelect: function (record) {
+        this.selectNode = record
+        if (this.onselected) {
+          this.onselected(record.record)
+        }
+      },
+      resizeRecordList: function (list, level, parentIsOk) {
+        let result = []
+        if (!level) level = 1
+        if (this.list) {
+          for (let i = 0; i < list.length; i++) {
+            if (parentIsOk || this.filterLike(list[i])) {
+              result.push({record: list[i], level: level, showChild: this.searchShowChild(list[i])})
+              if (list[i][this.childname]) {
+                let res = this.resizeRecordList(list[i][this.childname], level + 1, true)
+                if (res != null && res.length > 0) {
+                  result = result.concat(res)
+                }
+              }
+            } else {
+              if (list[i][this.childname]) {
+                let res = this.resizeRecordList(list[i][this.childname], level + 1, false)
+                if (res != null && res.length > 0) {
+                  result.push({record: list[i], level: level, showChild: true})
+                  result = result.concat(res)
+                }
+              } else {
+                continue
+              }
+            }
+          }
+        }
+        return result
+      },
+      resetRecordList: function (list, level, parentIsOk) {
+        let result = []
+        if (!level) level = 1
+        if (this.list) {
+          for (let i = 0; i < list.length; i++) {
+            if (parentIsOk || this.filterLike(list[i])) {
+              result.push({record: list[i], level: level, showChild: false})
+              if (list[i][this.childname]) {
+                let res = this.resetRecordList(list[i][this.childname], level + 1, true)
+                if (res != null && res.length > 0) {
+                  result = result.concat(res)
+                }
+              }
+            } else {
+              if (list[i][this.childname]) {
+                let res = this.resetRecordList(list[i][this.childname], level + 1, false)
+                if (res != null && res.length > 0) {
+                  result.push({record: list[i], level: level, showChild: true})
+                  result = result.concat(res)
+                }
+              } else {
+                continue
+              }
+            }
+          }
+        }
+        return result
+      },
+      searchShowChild: function (record) {
+        for (let i = 0; i < this.recordList.length; i++) {
+          if (this.recordList[i].record === record) {
+            return this.recordList[i].showChild
+          }
+        }
         return false
       }
     },
-
-    showRecordName: function (record) {
-      if (this.showfunc) {
-        return this.showfunc(record)
-      } else {
-        return record[this.showname]
-      }
-    },
-    treeNodeExtend: function (record) {
-      record.showChild = !record.showChild
-      this.selectNode = record
-      if (this.onselected) {
-        this.onselected(record.record)
-      }
-      if (this.onextend) {
-        this.onextend(record.record, record.showChild)
-      }
-    },
-    treeNodeSelect: function (record) {
-
-    },
-    resizeRecordList: function (list, level, parentIsOk) {
-      let result = []
-      if (!level) level = 1
-      if (this.list) {
-        for (let i = 0; i < list.length; i++) {
-          if (parentIsOk || this.filterLike(list[i])) {
-            result.push({record: list[i], level: level, showChild: this.searchShowChild(list[i])})
-            if (list[i][this.childname]) {
-              let res = this.resizeRecordList(list[i][this.childname], level + 1, true)
-              if (res != null && res.length > 0) {
-                result = result.concat(res)
-              }
-            }
-          } else {
-            if (list[i][this.childname]) {
-              let res = this.resizeRecordList(list[i][this.childname], level + 1, false)
-              if (res != null && res.length > 0) {
-                result.push({record: list[i], level: level, showChild: true})
-                result = result.concat(res)
-              }
-            } else {
-              continue
-            }
-          }
-        }
-      }
-      return result
-    },
-    resetRecordList: function (list, level, parentIsOk) {
-      let result = []
-      if (!level) level = 1
-      if (this.list) {
-        for (let i = 0; i < list.length; i++) {
-          if (parentIsOk || this.filterLike(list[i])) {
-            result.push({record: list[i], level: level, showChild: false})
-            if (list[i][this.childname]) {
-              let res = this.resetRecordList(list[i][this.childname], level + 1, true)
-              if (res != null && res.length > 0) {
-                result = result.concat(res)
-              }
-            }
-          } else {
-            if (list[i][this.childname]) {
-              let res = this.resetRecordList(list[i][this.childname], level + 1, false)
-              if (res != null && res.length > 0) {
-                result.push({record: list[i], level: level, showChild: true})
-                result = result.concat(res)
-              }
-            } else {
-              continue
-            }
-          }
-        }
-      }
-      return result
-    },
-    searchShowChild: function (record) {
-      for (let i = 0; i < this.recordList.length; i++) {
-        if (this.recordList[i].record === record) {
-          return this.recordList[i].showChild
-        }
-      }
-      return false
-    }
-  },
-  watch: {
-    list: {
-      handler: function (val, oldVal) {
-        // 刷新树图
-        this.recordList = this.resizeRecordList(this.list, 1, false)
+    watch: {
+      list: {
+        handler: function (val, oldVal) {
+          // 刷新树图
+          this.recordList = this.resizeRecordList(this.list, 1, false)
+        },
+        deep: true
       },
-      deep: true
-    },
-    filterText (oldValue, newValue) {
-      // 刷新树图
-      this.recordList = this.resetRecordList(this.list, 1, false)
+      filterText (oldValue, newValue) {
+        // 刷新树图
+        this.recordList = this.resetRecordList(this.list, 1, false)
+      }
     }
   }
-}
 </script>
 
 <style lang="less" scoped>
@@ -244,12 +243,12 @@ export default {
     outline: none;
     border-color: #409eff;
   }
- .icon-zengjia:before{
-   content: "\e604";
- }
- .cg-tree__main{
-   padding:0.6rem;
- }
+  .icon-zengjia:before{
+    content: "\e604";
+  }
+  .cg-tree__main{
+    padding:0.6rem;
+  }
   .cg-tree__node{
     line-height: 2.5rem;
     height:2.5rem;
